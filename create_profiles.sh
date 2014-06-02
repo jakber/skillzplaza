0ab0f23327920354d6197baa97649097cf4438ef
tnagelkerke <tom.nagelkerke@videoplaza.com>
2014-06-02 14:06:50 +0200

    create profiles shell file

---
+++ create_profiles.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+curl -X PUT http://127.0.0.1:5984/profile/Alexander -d '{
+			"name": "Alexander Altanis",
+            "primarySkills": ["Java", "Stuff"],
+            "secondarySkills": ["Scrum", "Excelling in Excel"],
+            "wishList": ["DJing", "Humility"],
+            "hobbiesOrFunFacts": ["Innebandy", "Diablo"],
+            "buyers": {"Java": "Alexander"},
+            "sellers": {"DJing": "Henry"}
+        }'
+curl -X PUT http://127.0.0.1:5984/profile/Eirini -d '{
+			"name": "Alexander Altanis",
+            "primarySkills": ["Java", "Stuff"],
+            "secondarySkills": ["Scrum", "Excelling in Excel"],
+            "wishList": ["DJing", "Humility"],
+            "hobbiesOrFunFacts": ["Innebandy", "Diablo"],
+            "buyers": {"Java": "Alexander"},
+            "sellers": {"DJing": "Henry"}
+        }'
+curl -X PUT http://127.0.0.1:5984/profile/Jakob -d '{
+			"name": "Alexander Altanis",
+            "primarySkills": ["Java", "Stuff"],
+            "secondarySkills": ["Scrum", "Excelling in Excel"],
+            "wishList": ["DJing", "Humility"],
+            "hobbiesOrFunFacts": ["Innebandy", "Diablo"],
+            "buyers": {"Java": "Alexander"},
+            "sellers": {"DJing": "Henry"}
+        }'
+curl -X PUT http://127.0.0.1:5984/profile/Stanislav -d '{
+			"name": "Alexander Altanis",
+            "primarySkills": ["Java", "Stuff"],
+            "secondarySkills": ["Scrum", "Excelling in Excel"],
+            "wishList": ["DJing", "Humility"],
+            "hobbiesOrFunFacts": ["Innebandy", "Diablo"],
+            "buyers": {"Java": "Alexander"},
+            "sellers": {"DJing": "Henry"}
+        }'
+curl -X PUT http://127.0.0.1:5984/profile/Tom -d '{
+			"name": "Alexander Altanis",
+            "primarySkills": ["Java", "Stuff"],
+            "secondarySkills": ["Scrum", "Excelling in Excel"],
+            "wishList": ["DJing", "Humility"],
+            "hobbiesOrFunFacts": ["Innebandy", "Diablo"],
+            "buyers": {"Java": "Alexander"},
+            "sellers": {"DJing": "Henry"}
+        }'
+curl -X PUT http://127.0.0.1:5984/profile/Per-Anders -d '{
+			"name": "Alexander Altanis",
+            "primarySkills": ["Java", "Stuff"],
+            "secondarySkills": ["Scrum", "Excelling in Excel"],
+            "wishList": ["DJing", "Humility"],
+            "hobbiesOrFunFacts": ["Innebandy", "Diablo"],
+            "buyers": {"Java": "Alexander"},
+            "sellers": {"DJing": "Henry"}
+        }'
